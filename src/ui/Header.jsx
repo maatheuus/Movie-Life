c94854872e0e06e9ff6ce84265ac1c179aae790f
MATHEUS <matts14smkd@gmail.com>
2024-07-23 19:10:52 -0300

    add: header to aplication

---
+++ src/ui/Header.jsx
@@ -0,0 +1,23 @@
+function Header() {
+  return (
+    <header>
+      <div>
+        <div>
+          <h1>Logo</h1>
+        </div>
+        <nav>
+          <ul>
+            <l1>Movies</l1>
+            <l1>Tv Shows</l1>
+          </ul>
+        </nav>
+        <div>
+          <form action=""></form>
+          <div>GUEST AREA</div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
