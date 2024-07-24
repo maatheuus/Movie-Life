15c4c5bf3de5776ffadbc30d92ff56d596002944
MATHEUS <matts14smkd@gmail.com>
2024-07-24 10:19:06 -0300

    add: add some styles to header

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,40 +1,59 @@
-import { Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import { Avatar } from "@mui/material";
 import { CiSearch } from "react-icons/ci";
 import defaultUser from "../assets/default-user.jpg";
-import { NavLink } from "react-router-dom";
+
 function Header() {
   return (
     <header className="fixed top-0 w-full h-16 bg-black bg-opacity-50 z-40">
       <div className="container mx-auto px-3 flex items-center h-full">
         <div>
-          <h1>Logo</h1>
+          <Link to="/">Logo</Link>
         </div>
 
-        <nav className="hidden md:flex items-center gap-1 ml-5">
-          <div>
-            <NavLink className="px-2 hover:text-neutral-100">Movies</NavLink>
-            <NavLink className="px-2 hover:text-neutral-100">Tv Shows</NavLink>
-          </div>
-        </nav>
+        <Navigation />
 
         <div className="ml-auto flex items-center gap-5">
-          <form action="" className="flex items-center gap-2">
+          <form className="flex items-center gap-2">
             <input
               type="text"
               placeholder="Search here..."
-              className="bg-transparent px-4 py-1 outline-none border-none hidden lg:block"
+              className="bg-transparent px-4 py-1 outline-none border-none hidden sm:block"
             />
             <button className="text-2xl text-white">
               <CiSearch />
             </button>
           </form>
-          <div>
-            <Avatar alt="Cindy Baker" src={defaultUser} />
-          </div>
+
+          <button>
+            <Avatar alt="Profile user" src={defaultUser} />
+          </button>
         </div>
       </div>
     </header>
+  );
+}
+
+function Navigation() {
+  const navigation = [
+    { label: "Tv Shows", href: "tv" },
+    { label: "Movie", href: "movie" },
+  ];
+
+  return (
+    <nav className="hidden sm:flex items-center gap-1 ml-5">
+      {navigation.map((nav) => {
+        return (
+          <NavLink
+            key={nav.label}
+            className="px-2 hover:text-neutral-50"
+            to={nav.href}
+          >
+            {nav.label}
+          </NavLink>
+        );
+      })}
+    </nav>
   );
 }
 
