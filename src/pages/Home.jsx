54c5ea41bcf2073deb791c482519c86edecb141a
MATHEUS <matts14smkd@gmail.com>
2024-07-24 22:06:47 -0300

    changind the layout and add some news code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,24 @@
-import { Outlet } from "react-router-dom";
-import Header from "../ui/Header";
-import MobileNavigation from "../ui/MobileNavigation";
+import BannerHome from "../ui/BannerHome";
+import { useFetchData } from "../hooks/useFetchData";
+import Card from "../ui/Card";
+import Heading from "../ui/Heading";
 
 function Home() {
+  const { data: trendingData } = useFetchData(
+    (state) => state.movieData.bannerData
+  );
   return (
-    <main className="pb-14 md:pb-0">
-      <Header />
-      <div className="min-h-[94h]">
-        <Outlet />
+    <div>
+      <BannerHome />
+      <div className="container mx-auto px-3 my-14">
+        <Heading as="h2" label="Treading Shows" className="mb-3" />
+        <div className="grid grid-cols-[repeat(auto-fit,230px)] gap-4">
+          {trendingData.map((data) => (
+            <Card key={data.id} data={data} />
+          ))}
+        </div>
       </div>
-      <MobileNavigation />
-    </main>
+    </div>
   );
 }
 
