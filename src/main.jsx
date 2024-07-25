7451989701ef1ff8d000ad9b724db83c0b2e70d4
MATHEUS <matts14smkd@gmail.com>
2024-07-24 22:04:38 -0300

    add defualt urls with axios

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import { Provider } from "react-redux";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import axios from "axios";
+import store from "./store/store.js";
 
 import "./index.css";
 
+const VITE_ACCESS_TOKEN = import.meta.env.VITE_ACCESS_TOKEN;
+
+/**setup axios */
+axios.defaults.baseURL = "https://api.themoviedb.org/3/";
+axios.defaults.headers.common["Authorization"] = `Bearer ${VITE_ACCESS_TOKEN}`;
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
