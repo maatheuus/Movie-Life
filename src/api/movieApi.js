b3e7b3c042cee71745d505f13bb996769dfa3c52
MATHEUS <matts14smkd@gmail.com>
2024-07-24 22:02:55 -0300

    add funtion to fetch the configurations to the app

--- src/api/movieApi.js
+++ src/api/movieApi.js
@@ -5,9 +5,20 @@ export async function fetchTrendingData() {
     const res = await axios.get("/trending/all/week");
 
     if (res.status !== 200) throw new Error("Error with the request");
-    console.log("res", res);
 
-    return res.data;
+    return res.data.results;
+  } catch (error) {
+    console.log("error", error);
+  }
+}
+
+export async function fetchConfig() {
+  try {
+    const res = await axios.get("/configuration");
+
+    if (res.status !== 200) throw new Error("Error with the request");
+
+    return res;
   } catch (error) {
     console.log("error", error);
   }
