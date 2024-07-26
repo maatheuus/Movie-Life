ec05a6022873afa5be3dc4597cb0143f8d2921f3
MATHEUS <matts14smkd@gmail.com>
2024-07-25 21:50:49 -0300

    test: commenting on the lines so as not to make too many requests

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,22 +2,66 @@ import BannerHome from "../ui/BannerHome";
 import { useFetchData } from "../hooks/useFetchData";
 import Card from "../ui/Card";
 import Heading from "../ui/Heading";
+import HorizontalScrollCard from "../ui/HorizontalScrollCard";
+import { useState } from "react";
+import { useFetch } from "../hooks/useFetch";
 
 function Home() {
   const { data: trendingData } = useFetchData(
     (state) => state.movieData.bannerData
   );
+
+  const { data: nowPlaying } = useFetch("/movie/now_playing");
+  const { data: topRatedMovies } = useFetch("/movie/top_rated");
+  const { data: mostPopularTvShows } = useFetch("/tv/popular");
+  const { data: topRatedTvShows } = useFetch("/tv/top_rated");
+  const { data: onAirShows } = useFetch("/tv/on_the_air");
+
+  const ficctionData = Array.from({ length: 15 });
+
   return (
     <div>
-      <BannerHome />
-      <div className="container mx-auto px-3 my-14">
-        <Heading as="h2" label="Treading Shows" className="mb-3" />
-        <div className="grid grid-cols-[repeat(auto-fit,230px)] gap-4">
-          {trendingData.map((data) => (
-            <Card key={data.id} data={data} />
-          ))}
-        </div>
+      {/* <BannerHome /> */}
+      {/**
+     * 
+       <HorizontalScrollCard
+        moviesData={trendingData}
+        heading="Treading"
+        trending
+      />
+      <div>
+        <Heading
+          as="h2"
+          label="Movies"
+          className="px-6 !text-2xl text-blue-700"
+        />
+        <HorizontalScrollCard
+          moviesData={nowPlaying}
+          heading="Now Playing Movies"
+        />
+        <HorizontalScrollCard
+          moviesData={topRatedMovies}
+          heading="Top Rated Movies"
+        />
       </div>
+
+      <div>
+        <Heading
+          as="h2"
+          label="Tv Shows"
+          className="px-6 !text-2xl text-blue-700"
+        />
+        <HorizontalScrollCard
+          moviesData={mostPopularTvShows}
+          heading="Popular Tv Shows"
+        />
+        <HorizontalScrollCard
+          moviesData={topRatedTvShows}
+          heading="Top Rated Tv Shows"
+        />
+        <HorizontalScrollCard moviesData={onAirShows} heading="On The Air" />
+      </div>
+     */}
     </div>
   );
 }
