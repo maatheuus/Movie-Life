c4d384b37e4292f9c8f5f34c7f83cee129391d30
MATHEUS <matts14smkd@gmail.com>
2024-07-26 21:18:24 -0300

    add mini spinner component for render in some spaces in the app

---
+++ src/ui/SpinnerMini.jsx
@@ -0,0 +1,7 @@
+import { ImSpinner2 } from "react-icons/im";
+
+function SpinnerMini() {
+  return <ImSpinner2 className="w-6 h-6 animate-spin text-[#2332a4f6] " />;
+}
+
+export default SpinnerMini;
