14eb42045e42da50e9bef348aa21dd7ae2045ab8
MATHEUS <matts14smkd@gmail.com>
2024-07-26 21:17:58 -0300

    add spinner component

---
+++ src/ui/Spinner.jsx
@@ -0,0 +1,9 @@
+import { ImSpinner9 } from "react-icons/im";
+
+function Spinner() {
+  return (
+    <ImSpinner9 className="w-14 h-14 animate-spin text-[#2332a4f6] absolute top-1/2 left-1/2" />
+  );
+}
+
+export default Spinner;
