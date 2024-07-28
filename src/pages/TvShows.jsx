ec158ea40fee5b78a7987eeb2251e76ddae5891e
MATHEUS <matts14smkd@gmail.com>
2024-07-28 16:43:07 -0300

    exclude the component

--- src/pages/TvShows.jsx
+++
@@ -1,8 +0,0 @@
-// import useFetch from "../hooks/useFetch";
-
-function TvShows() {
-  // const { data: allTvShowsData } = useFetch("discover/tv");
-  return <div>tvshows</div>;
-}
-
-export default TvShows;
