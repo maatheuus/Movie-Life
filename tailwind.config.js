f730e17c2ff44fec50136e212b84222281da77e4
MATHEUS <matts14smkd@gmail.com>
2024-07-28 16:50:48 -0300

    add helpers class

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        "blue-primary": "#2332a4f6",
+      },
+    },
   },
   plugins: [],
 };
