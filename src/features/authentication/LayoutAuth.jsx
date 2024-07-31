3b3905eefa5fbb1493126724b5ec0cc6214649e6
MATHEUS <matts14smkd@gmail.com>
2024-07-31 19:16:57 -0300

    refac the style and layout

--- src/features/authentication/LayoutAuth.jsx
+++ src/features/authentication/LayoutAuth.jsx
@@ -1,9 +1,9 @@
-import bgImage from "../../../public/movie_wallpaper.jpg";
+import bgImage from "../../../public/img/movie_wallpaper.jpg";
 import { RiMovie2Line } from "react-icons/ri";
 
 function LayoutAuth({ title, children }) {
   return (
-    <div className="bg-white dark:bg-gray-900">
+    <div className="bg-gray-900">
       <div className="flex justify-center h-screen relative">
         <div
           className="w-full bg-cover absolute top-0 h-full"
@@ -15,16 +15,16 @@ function LayoutAuth({ title, children }) {
         </div>
 
         <div className="flex items-center w-full max-w-lg mx-auto z-50">
-          <div className="bg-stone-950 flex-1 py-16 px-10 rounded-lg">
+          <div className="bg-stone-950 flex-1 py-10 px-10 rounded-lg">
             <div className="text-center">
               <div className="flex justify-center mx-auto">
                 <RiMovie2Line className="w-auto h-10 sm:h-8" />
               </div>
 
-              <p className="mt-3 text-neutral-100 text-xl">{title}</p>
+              <p className="mt-0.5 text-neutral-100 text-xl">{title}</p>
             </div>
 
-            <div className="mt-8">{children}</div>
+            <div>{children}</div>
           </div>
         </div>
       </div>
