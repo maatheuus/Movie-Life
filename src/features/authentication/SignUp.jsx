98d9d80e9cf6e97295ad47f1353991767b280a77
MATHEUS <matts14smkd@gmail.com>
2024-07-31 19:17:23 -0300

    split the component for formAuth

--- src/features/authentication/SignUp.jsx
+++ src/features/authentication/SignUp.jsx
@@ -1,88 +1,41 @@
 import { Button } from "@mui/material";
 import LayoutAuth from "./LayoutAuth";
 import { Link } from "react-router-dom";
-import {
-  FaUser,
-  FaArrowUpFromBracket,
-  FaEnvelope,
-  FaLock,
-  FaEye,
-} from "react-icons/fa6";
+import { useForm } from "react-hook-form";
+import FormAuth from "./FormAuth";
+import { useState } from "react";
 
 function SignUp() {
+  const [profilePhoto, setProfilePhoto] = useState("Profile Photo");
+  const { register, formState, handleSubmit, getValues } = useForm();
+  const { errors } = formState;
+
+  function onSubmit(data) {
+    console.log(data);
+    setProfilePhoto(data.profilePhoto[0].name);
+  }
+  console.log(profilePhoto);
+
   return (
     <LayoutAuth title="Sign up to save some movies">
-      <form className="w-full max-w-md">
-        <div className="relative flex items-center mt-8">
-          <span className="absolute">
-            <FaUser className="w-auto h-5 mx-3 text-gray-300 dark:text-gray-500" />
-          </span>
-
-          <input
-            type="text"
-            className="block w-full py-3 px-11 border rounded-lg placeholder-gray-600 bg-gray-900 text-gray-300 border-none border-gray-700 focus:border-blue-400 focus:ring-blue-400 focus:ring focus:ring-opacity-30"
-            placeholder="Username"
-          />
-        </div>
-
-        <label
-          for="dropzone-file"
-          className="flex items-center px-3 py-3 mx-auto mt-6 text-center border-2 border-dashed rounded-lg cursor-pointer border-gray-600 bg-gray-900"
-        >
-          <FaArrowUpFromBracket className="w-auto h-5 text-gray-500" />
-
-          <h2 className="mx-3 text-gray-400">Profile Photo</h2>
-
-          <input id="dropzone-file" type="file" className="hidden" />
-        </label>
-
-        <div className="relative flex items-center mt-6">
-          <span className="absolute">
-            <FaEnvelope className="w-auto h-5 mx-3 text-gray-500" />
-          </span>
-
-          <input
-            type="email"
-            className="block w-full py-3 px-11 border rounded-lg placeholder-gray-600 bg-gray-900 text-gray-300 border-none border-gray-700 focus:border-blue-400 focus:ring-blue-400 focus:ring focus:ring-opacity-30"
-            placeholder="Email address"
-          />
-        </div>
-
-        <div className="relative flex items-center mt-4">
-          <span className="absolute">
-            <FaLock className="w-auto h-5 mx-3 text-gray-500" />
-          </span>
-
-          <input
-            type="password"
-            className="block w-full py-3 px-11 border rounded-lg placeholder-gray-600 bg-gray-900 text-gray-300 border-none border-gray-700 focus:border-blue-400 focus:ring-blue-400 focus:ring focus:ring-opacity-30"
-            placeholder="Password"
-          />
-
-          <button className="absolute right-1">
-            <FaEye className="w-auto h-5 mx-3 text-gray-500" />
-          </button>
-        </div>
-
-        <div className="relative flex items-center mt-4">
-          <span className="absolute">
-            <FaLock className="w-auto h-5 mx-3 text-gray-500" />
-          </span>
-
-          <input
-            type="password"
-            className="block w-full py-3 px-11 border rounded-lg placeholder-gray-600 bg-gray-900 text-gray-300 border-none border-gray-700 focus:border-blue-400 focus:ring-blue-400 focus:ring focus:ring-opacity-30"
-            placeholder="Confirm Password"
-          />
-
-          <button className="absolute right-1">
-            <FaEye className="w-auto h-5 mx-3 text-gray-500" />
-          </button>
-        </div>
+      <form
+        className="w-full max-w-md flex flex-col gap-4"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <FormAuth
+          register={register}
+          errors={errors}
+          getValues={getValues}
+          name
+          photo
+          confirmPassword
+          profilePhoto={profilePhoto}
+        />
 
         <div className="mt-6">
           <Button
-            className="w-full !bg-[#2332a4f6] !py-2"
+            className="w-full !py-3 !bg-[#2332a4f6]"
+            type="submit"
             color="primary"
             variant="contained"
           >
