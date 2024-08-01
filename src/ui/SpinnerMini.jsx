24ca498daee123ebed544027dc7eb65493610e8a
MATHEUS <matts14smkd@gmail.com>
2024-08-01 19:10:23 -0300

    refactoring the spinner component

--- src/ui/SpinnerMini.jsx
+++ src/ui/SpinnerMini.jsx
@@ -1,7 +1,12 @@
 import { ImSpinner2 } from "react-icons/im";
+import { twMerge } from "tailwind-merge";
 
-function SpinnerMini() {
-  return <ImSpinner2 className="w-6 h-6 animate-spin text-[#2332a4f6] " />;
+function SpinnerMini({ className }) {
+  return (
+    <ImSpinner2
+      className={twMerge("w-6 h-6 animate-spin text-[#2332a4f6]", className)}
+    />
+  );
 }
 
 export default SpinnerMini;
