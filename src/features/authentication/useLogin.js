04297af0521f56e76bfad73ca6a2034c8f987eff
MATHEUS <matts14smkd@gmail.com>
2024-08-01 19:05:19 -0300

    add: handle with the login

---
+++ src/features/authentication/useLogin.js
@@ -0,0 +1,20 @@
+import { useNavigate } from "react-router-dom";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { login as loginAPI } from "../../api/authApi";
+
+export function useLogin() {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  const { isPending: isLoading, mutate: login } = useMutation({
+    mutationFn: (data) => loginAPI(data),
+    onSuccess: (user) => {
+      console.log("success", user);
+      queryClient.setQueryData(["user"], user);
+      navigate("/");
+    },
+    onError: () => console.log("Error"),
+  });
+
+  return { isLoading, login };
+}
