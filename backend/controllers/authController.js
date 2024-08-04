94f635ac709774f7e6e98160cd076750c28dacc4
MATHEUS <matts14smkd@gmail.com>
2024-08-04 18:16:59 -0300

    add: forgotPassword and resetPassword functions to reset the user's password once he clicks on the forgot password button

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,10 @@
 import jwt from "jsonwebtoken";
 import User from "../models/userModel.js";
-import catchAsync from "./../utils/catchAsync.js";
 import { promisify } from "util";
+import catchAsync from "./../utils/catchAsync.js";
+import AppError from "../utils/appError.js";
+import sendEmail from "../utils/email.js";
+import crypto from "crypto";
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -50,22 +53,15 @@ export const login = catchAsync(async (req, res, next) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    return res.status(400).json({
-      status: "fail",
-      message: "Please provide both email and password",
-    });
+    return next(new AppError("Please provide both email and password", 400));
   }
 
   const user = await User.findOne({ email }).select("+password");
   const correctPassword = await user.correctPassword(password, user.password);
 
   if (!user || !correctPassword) {
-    return res.status(401).json({
-      status: "fail",
-      message: "Incorrect email or password",
-    });
+    return next(new AppError("Incorrect email or password", 401));
   }
-
   createSendToken(user, "authenticated", 200, req, res);
 });
 
@@ -90,22 +86,77 @@ export const protect = catchAsync(async (req, res, next) => {
   }
 
   if (!token)
-    return res.status(401).json({
-      status: "fail",
-      message: "You are not logged in! Please log in to get access.",
-    });
+    return next(
+      new AppError("You are not logged in! Please log in to get access.", 401)
+    );
 
   const decode = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
   const currentUser = await User.findById(decode.id);
 
   if (!currentUser)
-    return res.status(401).json({
-      status: "fail",
-      message: "The user belonging to the token does not exist.",
-    });
+    return next(
+      new AppError("The user belonging to the token does not exist.", 401)
+    );
 
   req.user = currentUser;
   res.locals.user = currentUser;
   next();
+});
+
+export const forgotPassword = catchAsync(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user)
+    return next(new AppError("There is no user with this email address", 404));
+
+  const resetToken = await user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
+
+  const resetURL = `${req.protocol}://${req.get(
+    "host"
+  )}/api/v1/user/resetPassword/${resetToken}`;
+
+  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you dint't forget your password, please ignore this email!`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: "Your password reset token (valid for 10 minutes)",
+      message,
+    });
+
+    res.status(200).json({
+      status: "success",
+      message: "Token sent to email!",
+    });
+  } catch (error) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+
+    return next(
+      new AppError("There was an error sending the email. Try again later", 500)
+    );
+  }
+});
+
+export const resetPassword = catchAsync(async (req, res, next) => {
+  const { token } = req.params;
+  const hashedToken = crypto.createHash("sha256").update(token).digest("hex");
+
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() },
+  });
+
+  if (!user) return next(new AppError("Token is invalid or has expired", 400));
+
+  user.password = req.body.password;
+  user.confirmPassword = req.body.confirmPassword;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  await user.save();
+
+  createSendToken(user, "authenticated", 200, req, res);
 });
