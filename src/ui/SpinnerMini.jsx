ccbfb5bd1ee450a80dff43b0a757bab81d39a58b
MATHEUS <matts14smkd@gmail.com>
2024-08-20 19:24:04 -0300

    update class

--- src/ui/SpinnerMini.jsx
+++ src/ui/SpinnerMini.jsx
@@ -1,9 +1,9 @@
-import { ImSpinner2 } from "react-icons/im";
+import { LoaderCircle } from "lucide-react";
 import { twMerge } from "tailwind-merge";
 
 function SpinnerMini({ className }) {
   return (
-    <ImSpinner2
+    <LoaderCircle
       className={twMerge("w-6 h-6 animate-spin text-[#2332a4f6]", className)}
     />
   );
