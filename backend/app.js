4abc40072d65eae33f25846ae6ed8694cfb19686
MATHEUS <matts14smkd@gmail.com>
2024-08-20 18:56:13 -0300

    creating new route

--- backend/app.js
+++ backend/app.js
@@ -7,11 +7,11 @@ import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import { router as routerUser } from "./routes/userRoutes.js";
+import { router as routerMovie } from "./routes/movieRoutes.js";
 const app = e();
 
 // 1. Middlewares
 app.use(cors());
-
 if (process.env.NODE_DEV === "development") {
   app.use(morgan("dev"));
 }
@@ -20,6 +20,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // app.use(e.json({ limit: "100kb" }));
 app.use(cookieParser());
+
 app.use("/api/v1/user", routerUser);
+app.use("/api/v1/", routerMovie);
 
 export default app;
