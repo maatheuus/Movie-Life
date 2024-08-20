18454c2e1405421057cc93acdc506448b849c401
MATHEUS <matts14smkd@gmail.com>
2024-08-20 19:11:55 -0300

    update all the postersection with new data and hooks

--- src/features/details/PosterSection.jsx
+++ src/features/details/PosterSection.jsx
@@ -1,15 +1,37 @@
-import { useState } from "react";
-import { ButtonPlay, ButtonFavorite } from "../../ui/ButtonPlay";
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { ButtonFavorite, ButtonPlay } from "../../ui/ButtonPlay";
 import { useUser } from "../authentication/useUser";
-import { useNavigate } from "react-router-dom";
-function PosterSection({ imageURL, posterPath, onPlay }) {
-  const [favorite, setFavorite] = useState(false);
+import {
+  useFavoriteStatus,
+  useManageFavorite,
+} from "../favoriteMovie/useManageFavorite";
+
+function PosterSection({ imageURL, posterPath, onPlay, data }) {
+  const { explore } = useParams();
   const navigate = useNavigate();
-  const { isAuthenticated } = useUser();
-  function handleFavorite() {
+  const { isAuthenticated, user } = useUser();
+
+  const { addFav, removeFav, isLoading } = useManageFavorite();
+  const { favoriteStatus, setQueryData } = useFavoriteStatus();
+
+  function handleFavoriteClick(movieData) {
     if (!isAuthenticated) navigate("/login");
-    setFavorite((prev) => !prev);
+
+    const filterData = {
+      userId: user?._id,
+      mediaId: movieData?.id,
+      mediaType: explore,
+    };
+
+    if (favoriteStatus?.isFavorite) {
+      removeFav({ userId: user?._id, mediaId: data?.id });
+    } else addFav(filterData);
   }
+
+  useEffect(() => {
+    setQueryData({ userId: user?._id, mediaId: data?.id });
+  }, [setQueryData, data?.id, user?._id, isAuthenticated, navigate]);
 
   return (
     <div className="relative mx-auto lg:-mt-28 lg:mx-0 w-fit min-w-60 flex flex-col gap-4">
@@ -23,8 +45,11 @@ function PosterSection({ imageURL, posterPath, onPlay }) {
         <ButtonPlay renderPlay={onPlay} />
         <ButtonFavorite
           renderPlay={onPlay}
-          isFavorite={favorite}
-          renderFavorite={handleFavorite}
+          isLoading={isLoading}
+          isFavorite={favoriteStatus?.isFavorite}
+          renderFavorite={() => {
+            handleFavoriteClick(data);
+          }}
         />
       </div>
     </div>
