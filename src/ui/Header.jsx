4f476d85cb8e8d39a2b1762a6128cddba80cbcf3
MATHEUS <matts14smkd@gmail.com>
2024-08-20 19:22:24 -0300

    update component and add logo of the website

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,44 +1,77 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import { Avatar, IconButton } from "@mui/material";
-import { CiSearch } from "react-icons/ci";
+import { Search, Bookmark } from "lucide-react";
 
-import Navigation from "../features/navigation/Navigation";
+import logo from "../../public/img/logo.png";
 import defaultUser from "../assets/default-user.jpg";
+
 import { useUser } from "../features/authentication/useUser";
+import Navigation from "../features/navigation/Navigation";
 import FormSearch from "./FormSearch";
 import InputSearch from "./InputSearch";
 
 function Header() {
   const { isAuthenticated, user = [] } = useUser();
   const { photo } = user;
+  const [hover, setHover] = useState(false);
 
+  // console.log(isAuthenticated);
   return (
     <header className="fixed top-0 w-full h-16 bg-black bg-opacity-75 z-50">
-      <div className="container mx-auto px-3 flex items-center h-full">
+      <div className="container mx-auto px-6 sm:px-4 lg:px-10 flex items-center h-full">
         <div>
-          <Link to="/">Logo</Link>
+          <Link to="/">
+            <img
+              src={logo}
+              alt="logo the website"
+              className="w-10 h-10 rounded-full"
+              height={20}
+              width={20}
+            />
+          </Link>
         </div>
         <Navigation />
-        <div className="ml-auto flex items-center gap-5">
+        <div className="mx-auto flex items-center gap-2">
           <FormSearch className="hidden sm:flex">
-            <InputSearch />
             <IconButton
               aria-label="search"
               color="inherit"
               size="medium"
               sx={{ cursor: "default" }}
             >
-              <CiSearch />
+              <Search />
             </IconButton>
+            <InputSearch />
           </FormSearch>
+        </div>
 
+        <div className="flex items-center gap-3">
           {isAuthenticated ? (
-            <Link to="/account" arial-label="guest arear button">
-              <Avatar
-                alt="Profile user"
-                src={photo ? `../../../public/img/users/${photo}` : defaultUser}
-              />
-            </Link>
+            <>
+              <div className="hidden sm:block">
+                <IconButton
+                  aria-label="search"
+                  color="inherit"
+                  size="medium"
+                  sx={{ cursor: "default" }}
+                  onMouseEnter={() => setHover(true)}
+                  onMouseLeave={() => setHover(false)}
+                >
+                  <Link to="/favorite">
+                    {hover ? <Bookmark fill="#fff" /> : <Bookmark />}
+                  </Link>
+                </IconButton>
+              </div>
+              <Link to="/account" arial-label="guest arear button">
+                <Avatar
+                  alt="Profile user"
+                  src={
+                    photo ? `../../../public/img/users/${photo}` : defaultUser
+                  }
+                />
+              </Link>
+            </>
           ) : (
             <Link
               to="/login"
