2e1c0f051ee82085b27d884783882673e37d4729
MATHEUS <matts14smkd@gmail.com>
2024-08-20 19:18:54 -0300

    creating the forgot password page

--- src/pages/ForgotPasswordPage.jsx
+++ src/pages/ForgotPasswordPage.jsx
@@ -1,4 +1,4 @@
-import ForgotPassword from "../features/authentication/ForgotPassword";
+import ForgotPassword from "../features/authentication/resetPassword/ForgotPassword";
 
 function ForgotPasswordPage() {
   return <ForgotPassword />;
