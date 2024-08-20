d8ae08101327372c5f319fe4bf216d98d29330da
MATHEUS <matts14smkd@gmail.com>
2024-08-20 19:14:02 -0300

    update component

--- src/features/user/Account.jsx
+++ src/features/user/Account.jsx
@@ -1,13 +1,16 @@
+import { useUser } from "../authentication/useUser";
 import BasicInformation from "./BasicInformation";
 import DeleteAccount from "./DeleteAccount";
 import PasswordInformation from "./PasswordInformation";
 
 function Account() {
+  const { user, token } = useUser();
+
   return (
     <section className="grid grid-cols-1 gap-0 h-full px-8 py-20 md:gap-6 md:px-20 md:grid-cols-2">
-      <BasicInformation />
+      <BasicInformation user={user} />
       <PasswordInformation />
-      <DeleteAccount />
+      <DeleteAccount user={user} token={token} />
     </section>
   );
 }
