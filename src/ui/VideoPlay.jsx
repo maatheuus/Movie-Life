cedeb0fe350fa47d4e4477079501807987c54368
MATHEUS <matts14smkd@gmail.com>
2024-08-20 19:24:25 -0300

    update class and update the component

--- src/ui/VideoPlay.jsx
+++ src/ui/VideoPlay.jsx
@@ -1,18 +1,17 @@
-import { IoMdCloseCircleOutline } from "react-icons/io";
+import { CircleX } from "lucide-react";
 import { useFetch } from "../hooks/useFetch";
 
 function VideoPlay({ videoId, close, params }) {
   const { data: video } = useFetch(`${params}/${videoId}/videos`);
 
-  const displayVideo = video
-    .map((el) => el)
-    .filter((el) => el?.name.includes(" Trailer"));
+  const displayVideo =
+    video?.map((el) => el).filter((el) => el?.name.includes(" Trailer")) || [];
 
   return (
-    <section className="fixed bg-neutral-700 top-0 right-0 bottom-0 left-0 bg-opacity-50 flex justify-center items-center">
+    <section className="z-[9999] fixed bg-neutral-700 top-0 right-0 bottom-0 left-0 bg-opacity-50 flex justify-center items-center">
       <div className="bg-black w-full h-[80vh] max-w-screen-lg aspect-video rounded overflow-hidden relative p-6">
         <button onClick={close} className="absolute right-4 top-3 text-3xl">
-          <IoMdCloseCircleOutline />
+          <CircleX />
         </button>
 
         <iframe
