933a5dd3fc6a1149d47526476a5bfc0d0c24500a
MATHEUS <matts14smkd@gmail.com>
2024-08-20 18:55:27 -0300

    update configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,8 +13,7 @@ module.exports = {
   plugins: ["react-refresh"],
   rules: {
     "react/jsx-no-target-blank": "off",
-
-    "react/prop-types": ["disable"],
+    "react/prop-types": "warn",
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
