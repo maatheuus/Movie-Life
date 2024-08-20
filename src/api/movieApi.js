84c4df04f4228ac16f26f8bf8f4e8ca6d1cc1e58
MATHEUS <matts14smkd@gmail.com>
2024-08-20 19:03:28 -0300

    add all the functions for handle with the favorites in the front-end

--- src/api/movieApi.js
+++ src/api/movieApi.js
@@ -8,7 +8,7 @@ export async function fetchTrendingData() {
 
     return res.data.results;
   } catch (error) {
-    console.log("error", error);
+    throw new Error(error.message);
   }
 }
 
@@ -20,6 +20,53 @@ export async function fetchConfig() {
 
     return res;
   } catch (error) {
-    console.log("error", error);
+    throw new Error(error.message);
+  }
+}
+
+const URL = import.meta.env.VITE_URL_FAVORITES;
+
+export async function addFavorite(data) {
+  try {
+    const res = await axios.post(`${URL}/favorite`, data);
+
+    if (res.status !== 200) throw new Error("Error while add to the favorite");
+
+    return res.data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+export async function removeFavorite({ userId, mediaId }) {
+  const res = await axios.delete(`${URL}/favorite`, {
+    data: { userId, mediaId },
+  });
+  return res.data;
+}
+
+export async function getFavStatus(data) {
+  try {
+    const res = await axios.get(`${URL}/favorite-status`, {
+      params: data,
+    });
+
+    if (res.status !== 200) throw new Error("Error while add to the favorite");
+
+    return res.data || [];
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+export async function getFavMedia(data) {
+  try {
+    const res = await axios.get(`${URL}/favoriteMedia`, {
+      params: data,
+    });
+    if (res.status !== 200) throw new Error("Error while render the favorite");
+    return res.data || [];
+  } catch (error) {
+    throw new Error(error.message);
   }
 }
