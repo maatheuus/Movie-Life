d1fdfff07a22ad3393b6b6ebe74320567231d22f
MATHEUS <matts14smkd@gmail.com>
2024-08-20 19:22:05 -0300

    update component

--- src/ui/FormSearch.jsx
+++ src/ui/FormSearch.jsx
@@ -1,11 +1,12 @@
-import { IconButton } from "@mui/material";
-import { CiSearch } from "react-icons/ci";
-import InputSearch from "./InputSearch";
+import { twMerge } from "tailwind-merge";
 
 function FormSearch({ className, children }) {
   return (
     <form
-      className={`${className} items-center gap-2 border-none sm:border-b sm:border-b-zinc-100`}
+      className={twMerge(
+        "border-none bg-black bg-opacity-40 px-2  items-center rounded-3xl sm:border-b sm:border-b-zinc-100",
+        className
+      )}
     >
       {children}
     </form>
