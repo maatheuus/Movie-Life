e3992002841b195b00b90e2595700f4896298b0c
MATHEUS <matts14smkd@gmail.com>
2024-08-20 19:24:47 -0300

    update component

--- src/ui/MobileNavigation.jsx
+++ src/ui/MobileNavigation.jsx
@@ -1,19 +1,55 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { twMerge } from "tailwind-merge";
 import { mobileNavigation } from "../features/navigation/Navigation";
+import { useUser } from "../features/authentication/useUser";
 
 function MobileNavigation() {
+  const [value, setValue] = useState("");
+  const [showLabel, setShowLabel] = useState(false);
+  const { isAuthenticated } = useUser();
+
+  const handleChange = (newValue) => {
+    setValue(newValue);
+    setShowLabel(true);
+  };
+
+  const filterNavigation = mobileNavigation.filter((nav) => {
+    if (nav.label === "Favorites") {
+      return isAuthenticated;
+    }
+
+    return true;
+  });
+
   return (
     <section className="sm:hidden bg-black/30 backdrop-blur-3xl overflow-hidden fixed bottom-0 w-full h-14 z-[100]">
-      <div className="flex justify-between items-center h-full ">
-        {mobileNavigation.map((nav) => {
+      <div className="flex justify-around items-center h-full ">
+        {filterNavigation.map((nav) => {
+          const activeButton = value === nav.label;
+
           return (
             <NavLink
               key={nav.label}
               to={nav.href}
-              className="px-4 lg:px-8 flex h-full items-center flex-col justify-center"
+              className="flex h-full items-center flex-col justify-center"
             >
-              <div className="text-2xl">{nav.icon}</div>
-              <p className="text-sm">{nav.label}</p>
+              <button
+                onClick={() => handleChange(nav.label)}
+                className="flex flex-col items-center"
+              >
+                <span>{nav.icon}</span>
+                <span
+                  className={twMerge(
+                    "text-[11px] transition-all duration-500",
+                    showLabel && activeButton
+                      ? "opacity-100 translate-y-0"
+                      : "opacity-0 translate-y-10"
+                  )}
+                >
+                  {value}
+                </span>
+              </button>
             </NavLink>
           );
         })}
