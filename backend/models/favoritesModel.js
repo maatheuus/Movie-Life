f6c9eed36d3972e9c1bfa5c78dd343f9baceb0fc
MATHEUS <matts14smkd@gmail.com>
2024-08-20 18:59:58 -0300

    edit schema of the favorites

--- backend/models/favoritesModel.js
+++ backend/models/favoritesModel.js
@@ -1,12 +1,20 @@
 import { Schema, model } from "mongoose";
 
+// const favoriteSchema = new Schema({
+//   user: {
+//     type: {},
+//     required: [true, "Please, provide the current user!"],
+//   },
+//   favMovie: [],
+//   totalFavMovies: Number,
+// });
+
 const favoriteSchema = new Schema({
-  user: {
-    type: {},
-    required: [true, "Please, provide the current user!"],
-  },
-  favorites: [],
+  userId: { type: String, required: true },
+  mediaId: { type: String, required: true },
+  mediaType: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
 });
 
-const Favorites = model("Favorites", favoriteSchema);
+const Favorites = model("Favorite", favoriteSchema);
 export default Favorites;
